c23bc17e6523287477b2b4fb8e6cc0acfd304537
Priyavrat Misra <PriyavratMisra123@gmail.com>
2022-05-14 18:06:23 +0530

    add string init

--- leetcode/reshape-the-matrix.cpp
+++ leetcode/reshape-the-matrix.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     vector<vector<int>> matrixReshape(vector<vector<int>>& mat, int r, int c) {
         if (mat.size() * mat[0].size() == r * c) {
-            vector<vector<int>> v(r, vector<int> (c));
+            vector<vector<int>> v(r, vector<int>(c));
             int k = 0;
             for (vector<int>& row : mat) {
                 for (int& num : row) {
